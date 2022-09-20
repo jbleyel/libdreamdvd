a9ef79b7120b7b00e24762f7be7aff48ed4a8e2f
jbleyel <jbleyel@gmx.net>
2022-09-20 11:54:29 +0200

    include patches

--- main.h
+++ main.h
@@ -283,3 +283,6 @@ struct 	ddvd_resize_return	ddvd_resize_pixmap_xbpp_smooth(unsigned char *pixmap,
 struct	ddvd_resize_return	ddvd_resize_pixmap_1bpp(unsigned char *pixmap, int xsource, int ysource, int xdest, int ydest, int xoffset, int yoffset, int xstart, int xend, int ystart, int yend, int colors);
 struct	ddvd_resize_return	(*ddvd_resize_pixmap)(unsigned char *pixmap, int xsource, int ysource, int xdest, int ydest, int xoffset, int yoffset, int xstart, int xend, int ystart, int yend, int colors);
 #endif
+
+#define VIDEO_GET_FRAME_RATE       _IOR('o', 56, unsigned int)
+#define AUDIO_GET_PTS              _IOR('o', 19, __u64)
